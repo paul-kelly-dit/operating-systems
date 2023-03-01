3cfebe6056f9c845ea526c13b697a19d5a1c0ce3
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2023-03-01 15:58:39 +0000

    added a new script

--- bash-scripting/menu.sh
+++ bash-scripting/menu.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# The bash environment variable $PS3 is the prompt 
+# used by select . Set it to a new value and you'll get a new prompt.
 PS3="Select your language please: "
 
+# We should use the select command to create a simple menu 
+# in the terminal. Then, the command displays a list of options preceded by numbers.
+
+# select NAME [in WORDS ... ;] do COMMANDS; done
+# select repeatedly reads a number from standard input. Subsequently, if the number corresponds 
+# to a string’s position in WORDS, the command sets NAME to the respective text. Otherwise, nothing is set.
 select lng in Bash Java C++ Quit
 do
     case $lng in
