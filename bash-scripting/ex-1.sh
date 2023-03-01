33467dc2c8ebe60e669ca66666422ace15131ef7
paul-kelly-dit <36048467+paul-kelly-dit@users.noreply.github.com>
2023-03-01 15:47:38 +0000

    added some comments

--- bash-scripting/ex-1.sh
+++ bash-scripting/ex-1.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+# Add `#!/bin/bash` to the top of the script, which instructs the 
+# terminal everything found within the file is a part of a script
 
+
+# The first thing we’ll do is format the date as we like it using two variable like this
 formatted_datetime=$(date +%m-%d-%y:%H.%M.%S)
 formatted_date=$(date +%m-%d-%y)
+
+# Now we will output the date to the terminal
 echo "The date and time is " $formatted_datetime
 echo "The date is " $formatted_date
